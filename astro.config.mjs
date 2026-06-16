672006923bde52971789e84e84381e35d2bc99e8
yahircreativo <yahir.creativo@gmail.com>
2026-06-15 18:00:24 -0700

    Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://yahircreativo-art.github.io',
+  base: '/Vertice/'
 });
